bae372554f3158f07b53e4102837d3e1404ec7d2
bjyanxiaoyong <yanxiaoyong@jd.com>
2016-08-09 10:42:39 +0800

    Merge branch 'master' of https://github.com/cdyanhai/android_test
    Conflicts:
    	MyApplication/app/src/main/java/com/example/yanxiaoyong/myapplication/List2Activity.java
    	MyApplication/app/src/main/java/com/example/yanxiaoyong/myapplication/MainActivity.java

---
+++ MyApplication/app/src/main/java/com/example/yanxiaoyong/myapplication/MsgEvent.java
@@ -0,0 +1,20 @@
+package com.example.yanxiaoyong.myapplication;
+
+/**
+ * Created by yanxiaoyong on 2016/8/8.
+ */
+public class MsgEvent {
+    private String msg;
+
+    public MsgEvent(String msg) {
+        this.msg = msg;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
